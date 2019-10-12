c4a5838fc3eb64d535a67a991e2f0fb3f5d84b21
zhangke813 <528890545@qq.com>
2019-10-12 11:42:36 +0800

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/gaohonglei1219/neudemo.git
    
    Conflicts:
    	Hello2/src/com/neu/HiJeff.java

--- Hello2/src/com/neu/HiJeff.java
+++ Hello2/src/com/neu/HiJeff.java
@@ -2,9 +2,6 @@ package com.neu;
 
 public class HiJeff {
 	public static void main(String[] args) {
-		System.out.println("啊哈哈哈");
-		int a = 1;
-		System.out.println(a+3);
-		System.out.println("鸭儿哟");
+
 	}
 }

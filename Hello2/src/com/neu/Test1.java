1fedb6b0992edaf18579f1aec7683d972912a4a4
Administrator <Administrator@USER-20161021MG>
2019-10-12 15:16:19 +0800

    hahahahaha---

--- Hello2/src/com/neu/Test1.java
+++ Hello2/src/com/neu/Test1.java
@@ -2,6 +2,7 @@ package com.neu;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("ÄãºÃ");
+		System.out.println("ï¿½ï¿½ï¿½");
+		System.out.println("ssssss");
 	}
 }

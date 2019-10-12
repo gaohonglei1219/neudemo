9691c47982a2badee81f4e0b440d0c4b2165e68e
hp <hp@DESKTOP-JAIR1P3>
2019-10-12 17:14:15 +0800

    修改后

--- Hello2/src/com/neu/HelloYellowGun.java
+++ Hello2/src/com/neu/HelloYellowGun.java
@@ -3,10 +3,6 @@ package com.neu;
 public class HelloYellowGun {
 	public static void main(String[] args) {
 		System.out.println("ddj");
-<<<<<<< HEAD
 		System.out.println("hahaa");
-=======
-		System.out.println("小伙子");
->>>>>>> branch 'master' of https://github.com/gaohonglei1219/neudemo.git
 	}
 }

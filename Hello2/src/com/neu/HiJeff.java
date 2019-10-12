c43709aa0330363784df04ac44627017a009c6be
Administrator <Administrator@DAGE-PC>
2019-10-12 11:25:53 +0800

    # WARNING: head commit changed in the meantime
    hh

--- Hello2/src/com/neu/HiJeff.java
+++ Hello2/src/com/neu/HiJeff.java
@@ -3,8 +3,6 @@ package com.neu;
 public class HiJeff {
 	public static void main(String[] args) {
 		System.out.println("啊哈哈哈");
-		int a = 1;
-		System.out.println(a+3);
-		System.out.println("鸭儿哟");
+		System.out.println("sdsadasfa");
 	}
 }
